83db8ac2b34de5bae03aa29b846058c60aa57424
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-21 08:15:29 +0000

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """Basic Cache implementation Class
 """
 BaseCaching = __import__('base_caching').BaseCaching
@@ -11,16 +11,13 @@ class BasicCache(BaseCaching):
     Attributes:
         MAX_ITEMS: number of items that can be store in the cache
     """
-class BaseCaching:
-    def __init__(self):
-        self.cache_data = {}
-
-class BasicCache(BaseCaching):
     def put(self, key, item):
+        """ Add an item in the cache
+        """
         if key is not None and item is not None:
-            self.cache_data[key] = item
+            self.cache_data.update({key: item})
 
     def get(self, key):
-        if key is None or key not in self.cache_data:
-            return None
-        return self.cache_data[key]
+        """ Get an item by key
+        """
+        return self.cache_data.get(key, None)
