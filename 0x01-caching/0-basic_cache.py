5cd9c0ca35c27b53c786940b5a3760545f555586
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-21 08:12:06 +0000

    task 0

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
+"""Basic Cache implementation Class
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    A basic cache implementaion class
+
+    Attributes:
+        MAX_ITEMS: number of items that can be store in the cache
+    """
 class BaseCaching:
     def __init__(self):
         self.cache_data = {}
