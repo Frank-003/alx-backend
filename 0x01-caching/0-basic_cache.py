299a9a46280c5f21aa26de854ab4cb2e51ddc9e8
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-21 08:06:21 +0000

    task 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+class BaseCaching:
+    def __init__(self):
+        self.cache_data = {}
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
