16f5a07930fbd970c9591e1a78c7db2b879188ae
Frank-003 <oyiengofrankline49@gmail.com>
2024-08-21 08:41:05 +0000

    task 2

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -1,21 +1,33 @@
 #!/usr/bin/env python3
-from base_caching import BaseCaching
+class BaseCaching:
+    MAX_ITEMS = 4
+
+    def __init__(self):
+        """ Initialize the cache """
+        self.cache_data = {}
 
 class LIFOCache(BaseCaching):
     def __init__(self):
+        """ Initialize the LIFO Cache """
         super().__init__()
-        self.stack = []
+        self.stack = []  # to keep track of the order of inserted keys
 
     def put(self, key, item):
+        """ Add an item to the cache using LIFO algorithm """
         if key is None or item is None:
             return
+
         if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-            last_key = self.stack.pop()
+            last_key = self.stack.pop()  # get the last inserted key
             del self.cache_data[last_key]
             print(f"DISCARD: {last_key}")
+
         self.cache_data[key] = item
-        self.stack.append(key)
+        self.stack.append(key)  # push the new key onto the stack
 
     def get(self, key):
-        return self.cache_data.get(key, None)
+        """ Get an item by key """
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)
 
